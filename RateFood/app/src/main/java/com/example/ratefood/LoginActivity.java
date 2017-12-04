82fb4dc8f4fac36e1d2bdbedab8732b3267fa4ed
RobertBrunhage <robertbrunhage@gmail.com>
2017-12-04 12:46:16 +0100

    First Commit

---
+++ RateFood/app/src/main/java/com/example/ratefood/LoginActivity.java
@@ -0,0 +1,89 @@
+package com.example.ratefood;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener{
+
+    FirebaseAuth mAuth;
+    EditText EmailEditText, PasswordEditText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        EmailEditText = (EditText) findViewById(R.id.EmailEditText);
+        PasswordEditText = (EditText) findViewById(R.id.PasswordEditText);
+
+        findViewById(R.id.LoginTxt).setOnClickListener(this);
+        findViewById(R.id.LoginBtn).setOnClickListener(this);
+    }
+
+    private void userLogin(){
+
+        String email = EmailEditText.getText().toString().trim();
+        String password = PasswordEditText.getText().toString().trim();
+
+        if(email.isEmpty()){
+            EmailEditText.setError("Email is required");
+            EmailEditText.requestFocus();
+            return;
+        }
+        if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            EmailEditText.setError("Please enter a valid email");
+            EmailEditText.requestFocus();
+            return;
+        }
+        if(password.isEmpty()){
+            PasswordEditText.setError("Password is required");
+            PasswordEditText.requestFocus();
+            return;
+        }
+        if(password.length() < 6){
+            PasswordEditText.setError("Minimum length of password should be 6");
+            PasswordEditText.requestFocus();
+            return;
+        }
+
+        mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    Intent i = new Intent(LoginActivity.this, MainActivity.class);
+                    i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                    startActivity(i);
+                    finish();
+                }else{
+                    Toast.makeText(getApplicationContext(), task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.LoginTxt:
+                startActivity(new Intent(this, RegisterActivity.class));
+                break;
+
+            case R.id.LoginBtn:
+                userLogin();
+                break;
+        }
+    }
+}
---
+++ RateFood/app/src/main/java/com/example/ratefood/RegisterActivity.java
@@ -0,0 +1,96 @@
+package com.example.ratefood;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
+
+public class RegisterActivity extends AppCompatActivity implements View.OnClickListener {
+
+    EditText EmailEditText, PasswordEditText;
+
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        findViewById(R.id.RegisterTxt).setOnClickListener(this);
+
+        EmailEditText = (EditText) findViewById(R.id.EmailEditText);
+        PasswordEditText = (EditText) findViewById(R.id.PasswordEditText);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        findViewById(R.id.RegisterBtn).setOnClickListener(this);
+
+    }
+
+    private void registerUser(){
+        String email = EmailEditText.getText().toString().trim();
+        String password = PasswordEditText.getText().toString().trim();
+
+        if(email.isEmpty()){
+            EmailEditText.setError("Email is required");
+            EmailEditText.requestFocus();
+            return;
+        }
+        if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            EmailEditText.setError("Please enter a valid email");
+            EmailEditText.requestFocus();
+            return;
+        }
+        if(password.isEmpty()){
+            PasswordEditText.setError("Password is required");
+            PasswordEditText.requestFocus();
+            return;
+        }
+        if(password.length() < 6){
+            PasswordEditText.setError("Minimum length of password should be 6");
+            PasswordEditText.requestFocus();
+            return;
+        }
+
+        mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    Intent i = new Intent(RegisterActivity.this, MainActivity.class);
+                    i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                    startActivity(i);
+                }else {
+                    if(task.getException() instanceof FirebaseAuthUserCollisionException){
+                        Toast.makeText(getApplicationContext(), "You are already registered", Toast.LENGTH_SHORT).show();
+
+                    } else{
+                        Toast.makeText(getApplicationContext(), task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+
+                    }
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.RegisterTxt:
+                startActivity(new Intent(this, LoginActivity.class));
+                break;
+            case R.id.RegisterBtn:
+                registerUser();
+                break;
+        }
+    }
+}
